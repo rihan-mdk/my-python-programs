82208f54ce2c931d7774d536047b0cccb3395e12
rihan <innurihan286@gmail.com>
2025-10-28 05:00:20 +0530

    4 commited

---
+++ grt.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the fst number"))
+num2 = int(input("Enter the scnd number"))
+if(num1 > num2) :
+    print(num1, "is greater")
+elif(num2 > num1) :
+    print(num2, "is greater")
+else :
+    print("both are equal")
