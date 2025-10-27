47f7e1e3409c3c0b83d85aded7ca4f963aef9025
rihan <innurihan286@gmail.com>
2025-10-28 04:43:28 +0530

    3 commited

---
+++ num.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the number :"))
+if(num1%2==0):
+   print("THE ENTERED NUMBER ",num1,"IS EVEN...")
+else :
+   print("THE ENTERED NUMBER",num1,"IS ODD...")
+print("THANKYOU ...")
